98ea9a58a31177f4d265ad007d3d4212a4833e49
CodeWithBry <bryanagustinpajarillaga@gmail.com>
2026-01-12 16:40:57 +0800

    first commit

---
+++ public/login/login.js
@@ -0,0 +1,78 @@
+const labels = document.querySelectorAll(".inputs > label");
+const labelTexts = document.querySelectorAll(".label-text");
+const inputs = document.querySelectorAll("input");
+const emailInput = document.getElementById("username-input");
+const passInput = document.getElementById("password-input");
+const errorMess = document.querySelectorAll(".error")
+const changeVisibilityBtn = document.getElementById("changeVisibility");
+const submitButton = document.getElementById("submit-btn");
+
+let passwordVisibility = false;
+
+async function getUserLoggedIn() {
+    const email = emailInput.value;
+    const password = passInput.value;
+
+    if (!email || !password) {
+        numOfError += 1;
+        if (!email) errorMess[0].innerHTML == "Fill This Form!";
+        if (!password) errorMess[1].innerHTML == "Fill This Form!";
+        return;
+    }
+
+    try {
+        console.log("getResponse!...")
+        const response = await fetch("http://localhost:3000/api/get-logged-in", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password })
+        })
+
+        const getData = await response.json();
+        localStorage.setItem("user", JSON.stringify(getData.result))
+        console.log(getData.result)
+        if (getData?.message) {
+            alert(getData.message)
+            window.location.href = "http://localhost:3000/";
+        }
+    } catch (error) {
+        alert(error)
+    }
+}
+
+labels.forEach((label, index) => {
+    const input = label.querySelector("input");
+
+    input.addEventListener("focus", () => {
+        labelTexts[index].classList.add("focus");
+    });
+
+    input.addEventListener("blur", () => {
+        // Remove class if input is empty
+        if (!input.value) {
+            labelTexts[index].classList.remove("focus");
+        }
+    });
+});
+
+inputs.forEach((e, i) => {
+    e.addEventListener("change", () => {
+        if (e.value.length <= 7) {
+            errorMess[i].innerHTML = "Character length must be greater than 7 characters!"
+        } else {
+            errorMess[i].innerHTML = ""
+        }
+    })
+})
+
+changeVisibilityBtn.addEventListener("click", () => {
+    passwordVisibility = !passwordVisibility;
+    const i = changeVisibilityBtn.querySelector("i");
+    if (i.classList.contains("fa-eye-slash") && passwordVisibility) {
+        i.classList.replace("fa-eye-slash", "fa-eye")
+    } else i.classList.replace("fa-eye", "fa-eye-slash")
+    passInput.type = passwordVisibility ? "text" : "password";
+})
+
+
+submitButton.addEventListener("click", getUserLoggedIn)
---
+++ public/signup/signup.js
@@ -0,0 +1,97 @@
+const labels = document.querySelectorAll(".inputs > label");
+const inputs = document.querySelectorAll("input")
+const errorMess = document.querySelectorAll(".error")
+const labelTexts = document.querySelectorAll(".label-text");
+const emailInput = document.getElementById("username-input");
+const passInput = document.getElementById("password-input");
+const confirmPassInput = document.getElementById("confirm-password-input");
+const changeVisibilityBtn = document.querySelectorAll(".changeVisibility");
+const submitButton = document.getElementById("submit-btn");
+
+const numOfError = 0;
+let passwordVisibility = false;
+let confirmPasswordVisibility = false;
+
+labels.forEach((label, index) => {
+    const input = label.querySelector("input");
+
+    input.addEventListener("focus", () => {
+        labelTexts[index].classList.add("focus");
+    });
+
+    input.addEventListener("blur", () => {
+        // Remove class if input is empty
+        if (!input.value) {
+            labelTexts[index].classList.remove("focus");
+        }
+    });
+});
+
+async function createUser() {
+    const email = emailInput.value;
+    const password = passInput.value;
+    const confirmPassword = confirmPassInput.value;
+
+    if (!email || !password || !confirmPassInput) {
+        numOfError += 1;
+        if (!email) errorMess[0].innerHTML == "Fill This Form!";
+        if (!password) errorMess[1].innerHTML == "Fill This Form!";
+        if (!confirmPassword) errorMess[2].innerHTML == "Fill This Form!";
+        return;
+    }
+
+    if (password != confirmPassword) {
+        if (!password) errorMess[1].innerHTML == "The password doesn't match!";
+        if (!confirmPassword) errorMess[2].innerHTML == "The password doesn't match!";
+        return;
+    }
+
+    if (password == confirmPassword) {
+        try {
+            const response = await fetch("http://localhost:3000/api/create-user", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password })
+            })
+
+            const getMessage = await response.json();
+
+            if (getMessage?.message) {
+                alert(getMessage.message)
+                window.location.href = "http://localhost:3000/login";
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+submitButton.addEventListener("click", createUser)
+inputs.forEach((e, i) => {
+    e.addEventListener("change", () => {
+        if (e.value.length <= 7) {
+            errorMess[i].innerHTML = "Character length must be greater than 7 characters!"
+        } else {
+            errorMess[i].innerHTML = ""
+        }
+    })
+})
+
+changeVisibilityBtn.forEach((e, i) => {
+    e.addEventListener("click", () => {
+        if (i == 0) {
+            passwordVisibility = !passwordVisibility;
+            const i = e.querySelector("i");
+            if(i.classList.contains("fa-eye-slash") && passwordVisibility) {
+                i.classList.replace("fa-eye-slash", "fa-eye")
+            } else i.classList.replace("fa-eye", "fa-eye-slash")
+            passInput.type = passwordVisibility ? "text" : "password";
+        } else {
+            confirmPasswordVisibility = !confirmPasswordVisibility;
+            confirmPassInput.type = confirmPasswordVisibility ? "text" : "password";
+        }
+    })
+})
+
